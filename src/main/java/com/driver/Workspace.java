5240e6544208fc6d01d3876c6aff5d665dde6b72
unknown <hemant38056@gmail.com>
2022-12-24 23:47:44 +0530

    Submission2

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -11,6 +11,7 @@ public class Workspace extends Gmail{
 
     private ArrayList<Meeting> calendar; // Stores all the meetings
 
+
     public Workspace(String emailId) {
         // The inboxCapacity is equal to the maximum value an integer can store.
         super(emailId, Integer.MAX_VALUE);
@@ -29,6 +30,26 @@ public class Workspace extends Gmail{
         // 1. At a particular time, you can be present in at most one meeting
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
-        return 3;
+        Collections.sort(calendar, (a, b) -> {
+            return a.getStartTime().compareTo(b.getStartTime());
+        });
+        int count = 0;
+        for(int i = 0; i < calendar.size(); i++){
+            int temp = 1;
+            int index = i;
+            Meeting previous = calendar.get(index);
+            for(int j = i + 1; j < calendar.size(); j++){
+                Meeting current = calendar.get(j);
+                LocalTime time1 = current.getStartTime();
+                LocalTime time2 = previous.getEndTime();
+                int flag = time1.compareTo(time2);
+                if(flag >= 0){
+                    temp++;
+                    index = j;
+                }
+                count = Math.max(count, temp);
+            }
+        }
+        return count;
     }
 }
