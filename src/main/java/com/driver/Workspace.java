8d02b2e6762ce30fe95ca1707abd431a37fc4876
unknown <hemant38056@gmail.com>
2022-12-25 00:07:01 +0530

    Submission4

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -31,6 +31,9 @@ public class Workspace extends Gmail{
         // 2. If you want to attend a meeting, you must join it at its start time and leave at end time.
         // Example: If a meeting ends at 10:00 am, you cannot attend another meeting starting at 10:00 am
         Collections.sort(calendar, (a, b) -> {
+            if(a.getStartTime().compareTo(b.getStartTime()) == 0){
+                return a.getEndTime().compareTo(b.getEndTime());
+            }
             return a.getStartTime().compareTo(b.getStartTime());
         });
         int count = 0;
@@ -44,7 +47,7 @@ public class Workspace extends Gmail{
                 LocalTime time2 = previous.getEndTime();
                 int flag = time1.compareTo(time2);
                 if(flag > 0){
-                    temp++;
+                    temp = temp + 1;
                     index = j;
                 }
                 count = Math.max(count, temp);
